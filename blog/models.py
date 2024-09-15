f0375851efb6aa349d2d204dc88f8fc56b278f38
sumi <ishtiaq.khan1354@gmail.com>
2024-09-16 00:04:07 +0100

    Blog project made w/ django, first commit

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+# >> model for posting within blog <<
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE) # >> link to another module <<
+    title = models.CharField(max_length = 200) # >> define text w/ char limit <<
+    text = models.TextField() # >> define text w/o char limit <<
+    created = models.DateTimeField(default=timezone.now) # >> date and time of creation <<
+    published = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published = timezone.now()
+        self.save()
+
+        def __str__(self):
+            return self.title
