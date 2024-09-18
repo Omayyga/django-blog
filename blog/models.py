ac3ca4171a9442c89952fbf0d6c7bfe79a1e1e5f
sumi <ishtiaq.khan1354@gmail.com>
2024-09-18 03:37:00 +0100

    HTML Changes

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,8 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE) # >> link to another module <<
     title = models.CharField(max_length = 200) # >> define text w/ char limit <<
     text = models.TextField() # >> define text w/o char limit <<
-    created = models.DateTimeField(default=timezone.now) # >> date and time of creation <<
-    published = models.DateTimeField(blank=True, null=True)
+    created = models.DateTimeField(default = timezone.now) # >> date and time of creation <<
+    published = models.DateTimeField(blank = True, null = True)
 
     def publish(self):
         self.published = timezone.now()
