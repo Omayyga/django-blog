3eb00ca55d9ba45e8f5eec6d4ce03c3af16dac77
sumi <ishtiaq.khan1354@gmail.com>
2024-09-20 23:49:32 +0100

    Modified templates; Displays posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post # >> the . before models means current directory/application <<
+from django.utils import timezone
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {}) # >> takes request and calls function to render the template <<
+    posts = Post.objects.filter(published__lte = timezone.now()).order_by('published')
+    return render(request, 'blog/post_list.html', {'posts' : posts}) # >> takes request and calls function to render the template <<
