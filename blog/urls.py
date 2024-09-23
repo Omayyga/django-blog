1fea4daa3d1804ca4f43b15f8b285e816ba16157
sumi <ishtiaq.khan1354@gmail.com>
2024-09-23 02:25:32 +0100

    + Edit/Post Function within the site if user is authenticated

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name = 'post_list'),
     path('post/<int:pk>/', views.post_detail, name = 'post_detail'), # >> adds to the URL with /post and follows post with the post number i.e. 127.0.0.1:8000/post/1/ <<
+    path('post/new/', views.post_new, name = 'post_new'),
+    path('post/<int:pk>/edit/', views.edit_post, name = 'edit_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post # >> the . before models means current directory/application <<
 from django.utils import timezone
+from .forms import Post_Form
 
 
 def post_list(request):
@@ -12,3 +13,31 @@ def post_detail(request, pk):
 
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = Post_Form(request.POST)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.published = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+        
+    else:
+        form = Post_Form()
+    return render(request, 'blog/add_post.html', {'form': form})
+
+def edit_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = Post_Form(request.POST, instance = post)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.published = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = Post_Form(instance=post)
+    return render (request, 'blog/add_post.html', {'form': form})
